54dad1f67fa390174bd4aa2301073c95e6237d01
Nathan Wu <nathan@joomlacreator.com>
2018-07-17 18:06:22 +0800

    refact code

--- web/index.php
+++ web/index.php
@@ -10,8 +10,6 @@ use App\AspectKernel;
 use Silex\Application;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
-define('DEBUG', true);
-ini_set('display_errors', (int)DEBUG);
 $app = new Application();
 require dirname(__DIR__) . '/config/config.php';
 require dirname(__DIR__) . '/config/constants.php.php';
